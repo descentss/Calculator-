6acea0def3e225f912799ec39b9056c25585374e
descentss <engspesee@gmail.com>
2023-07-15 15:53:00 +0900

    Updated HTML and CSS

---
+++ script.js
@@ -0,0 +1,26 @@
+let firstNumber = "";
+let secondNumber = "";
+let currentOperation = null;
+let shouldReset = false;
+
+// Assigning a variable to each Calculator Button
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalButton = document.getElementById("equalBtn");
+const allClearButton = document.getElementById("allClearBtn");
+const deleteButton = document.getElementById("deleteBtn");
+const decimalButton = document.getElementById("decimalBtn");
+const previousOperationScreen = document.querySelector(".screen-previous");
+const currentOperationScreen = document.querySelector(".screen-current");
+
+// Add Event Listeners on Equal, Clear, Delete and Decimal Buttons
+equalButton.addEventListener("click", )
+allClearButton.addEventListener("click", allClear)
+deleteButton.addEventListener("click", delete)
+decimalButton.addEventListener("click", appendDecimal)
+
+function Clear() {
+    
+}
+
+
